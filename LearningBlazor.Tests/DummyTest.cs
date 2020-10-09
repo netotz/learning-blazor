bbcabed90ce4ec0eabc23417cccf4e637acb240c
netotz <neto.otz@hotmail.com>
2020-10-09 17:04:33 -0500

    Add xUnit tests project

---
+++ LearningBlazor.Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+﻿
+using Xunit;
+
+namespace LearningBlazor.Tests {
+    public class DummyTest {
+        
+        [Fact]
+        public void Add_AddingZero_ReturnsSameNumber() {
+            var onePlusZero = 1 + 0;
+            Assert.Equal(0, onePlusZero);
+        }
+    }
+}
