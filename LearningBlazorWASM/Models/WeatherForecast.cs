e9e20847e6d0d80b547ab60f1e10d6d119586c70
netotz <neto.otz@hotmail.com>
2020-10-09 17:50:55 -0500

    Add validation annotations

--- LearningBlazorWASM/Models/WeatherForecast.cs
+++ LearningBlazorWASM/Models/WeatherForecast.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LearningBlazorWASM.Models {
     public class WeatherForecast {
 
+        [Required]
         public DateTime Date { get; set; }
 
         public string Summary { get; set; }
 
+        [Required]
+        [Range(-100, 100, ErrorMessage = "Temperature exceeds limit.")]
         public int CelsiusTemperature { get; set; }
 
         public int FahrenheitTemperature =>  (int)(CelsiusTemperature * (9.0 / 5.0)) + 32;
