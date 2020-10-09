80dbba43899728272f7f6daac61d26d4874f8c96
netotz <neto.otz@hotmail.com>
2020-10-09 17:57:08 -0500

    Add WeatherForecast test

---
+++ LearningBlazor.Tests/WeatherTest.cs
@@ -0,0 +1,20 @@
+﻿using LearningBlazorWASM.Models;
+
+using Xunit;
+
+namespace LearningBlazor.Tests {
+    public class WeatherTest {
+
+        /// <summary>
+        /// It's a fact that 0 °C = 32 °F.
+        /// </summary>
+        [Fact]
+        public void GetFahrenheit_FromCelsius_SuccessfulConversion() {
+            var weatherForecast = new WeatherForecast {
+                CelsiusTemperature = 0
+            };
+            var fahrenheit = weatherForecast.FahrenheitTemperature;
+            Assert.Equal(32, fahrenheit);
+        }
+    }
+}
